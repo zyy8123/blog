be971f3fb3db2667e5e3d5defb50a3515f49118d
poixao <1412634436@qq.com>
2022-11-15 21:26:30 +0800

    Initial commit

---
+++ src/assets/js/config.js
@@ -0,0 +1,7 @@
+export default {
+  QQ_APP_ID: "102029440",
+  QQ_REDIRECT_URI: "https://www.heyzyy.com/oauth/login/qq",
+  WEIBO_APP_ID: "1732000495",
+  WEIBO_REDIRECT_URI: "https://www.heyzyy.com/oauth/login/weibo",
+  TENCENT_CAPTCHA: "2088053498"
+};
---
+++ vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  // 解决跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
+    disableHostCheck: true
+  },
+  productionSourceMap: false,
+  css: {
+    extract: true,
+    sourceMap: false
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: "favicon.ico",
+      favicon16: "favicon.ico",
+      appleTouchIcon: "favicon.ico",
+      maskIcon: "favicon.ico",
+      msTileImage: "favicon.ico"
+    }
+  }
+};
